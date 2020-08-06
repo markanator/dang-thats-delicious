6b254d7eefa0d42b95f7544de199a44b3f7b0ff1
Mark Ambro <wowmarkambro@gmail.com>
2020-08-06 00:02:26 -0500

    google maps stuff working

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,6 @@
-import '../sass/style.scss';
+import "../sass/style.scss";
 
-import { $, $$ } from './modules/bling';
+import { $, $$ } from "./modules/bling";
+import autoComplete from "./modules/autoComplete";
+
+autoComplete($("#address"), $("#lat"), $("#lng"));
