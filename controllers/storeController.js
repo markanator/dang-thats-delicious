9decc30f3875cba688082e782a58ad07b65d1544
Mark Ambro <wowmarkambro@gmail.com>
2020-08-07 17:17:30 -0500

    image uploading working

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,63 +1,87 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith("image/");
+        if (isPhoto) {
+            next(null, true);
+        } else {
+            next({ message: "That filetype isn't allowed!" }, false);
+        }
+    },
+};
 
 exports.homePage = (req, res) => {
-    // console.log(req.name);
     res.render("index");
 };
 
 exports.addStore = (req, res) => {
-    res.render("editStore", {
-        title: "Add Store",
-    });
+    res.render("editStore", { title: "Add Store" });
+};
+
+exports.upload = multer(multerOptions).single("photo");
+
+exports.resize = async (req, res, next) => {
+    // check if there is no new file to resize
+    if (!req.file) {
+        next(); // skip to the next middleware
+        return;
+    }
+    const extension = req.file.mimetype.split("/")[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // now we resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // once we have written the photo to our filesystem, keep going!
+    next();
 };
 
 exports.createStore = async (req, res) => {
-    // create a var to pass req into schema
-    // connect to DB and await for response
-    // immediately save to gain access to values
-    // prettier-ignore
-    const store = await (new Store(req.body)).save();
-    // notify user that store was created
+    const store = await new Store(req.body).save();
     req.flash(
         "success",
         `Successfully Created ${store.name}. Care to leave a review?`
     );
-    // push user to store page
     res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
-    // 1. Query DB for all stores
+    // 1. Query the database for a list of all stores
     const stores = await Store.find();
-    // console.log(stores);
     res.render("stores", { title: "Stores", stores });
 };
 
 exports.editStore = async (req, res) => {
-    // 1. find store given ID
+    // 1. Find the store given the ID
     const store = await Store.findOne({ _id: req.params.id });
-    // console.log(store);
     // 2. confirm they are the owner of the store
     // TODO
-    // 3. render out edit form so user can update
+    // 3. Render out the edit form so the user can update their store
     res.render("editStore", { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
-    // 1. find and update store
+    // set the location data to be a point
+    req.body.location.type = "Point";
+    // find and update the store
     const store = await Store.findOneAndUpdate(
         { _id: req.params.id },
         req.body,
         {
-            new: true, //return new store instead of old one
-            runValidators: true, // force model to run required validators
+            new: true, // return the new store instead of the old one
+            runValidators: true,
         }
     ).exec();
     req.flash(
         "success",
-        `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`
+        `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`
     );
     res.redirect(`/stores/${store._id}/edit`);
-    // 2. redirect to store and notify them that it worked!
+    // Redriect them the store and tell them it worked
 };
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const router = express.Router();
-
 const storeController = require("../controllers/storeController");
 const { catchErrors } = require("../handlers/errorHandlers");
 
-// Do work here
 router.get("/", catchErrors(storeController.getStores));
 router.get("/stores", catchErrors(storeController.getStores));
 router.get("/add", storeController.addStore);
-router.post("/add", catchErrors(storeController.createStore));
-router.post("/add/:id", catchErrors(storeController.updateStore));
+
+router.post(
+    "/add",
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.createStore)
+);
+
+router.post(
+    "/add/:id",
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.updateStore)
+);
+
 router.get("/stores/:id/edit", catchErrors(storeController.editStore));
 // ^^^ with high order function for ASYNC/AWAIT
 
