22a7aba848c3872ae71831528785b5dcccc99734
Mark Ambro <wowmarkambro@gmail.com>
2020-08-25 12:55:38 -0500

    pagination added, sorting stores by created

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // STORES STUFF
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/page/:page', catchErrors(storeController.getStores));
 
 router.get('/add', authController.isLoggedin, storeController.addStore);
 
